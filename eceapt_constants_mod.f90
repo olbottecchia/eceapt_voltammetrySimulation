5f7e5479146cdcfc14832f8cc4733b5d196c5d53
olbottecchia <otavio@ufu.br>
2026-09-14 20:24:36 -0300

    Delete eceapt_constants_mod.f90

--- eceapt_constants_mod.f90
+++
@@ -1,9 +0,0 @@
-module eceapt_constants
-implicit none
-
-integer, parameter :: nxmax = 1300                                           ! Maximal number of elements of concentration profile
-real, parameter :: lambda = 0.3
-character(len=14), parameter :: inputpar = 'parametros.dat'                            ! File name (without the .txt suffix) where the input parameters are to be found.
-real, parameter :: alfa = 0.5
-real, parameter, dimension(10) :: one_0 = (/1., 0.,0.,0.,0.,0.,0.,0.,0.,0./)
-end module eceapt_constants
